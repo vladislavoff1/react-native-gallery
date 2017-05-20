cbd71fabdadc94e56bd08c6909e9c20ef2553e68
kyunghoon <kyunghoon@gmail.com>
2017-05-20 14:07:33 -0400

    removed neighbor loading

--- library/Gallery.js
+++ library/Gallery.js
@@ -270,11 +270,6 @@ export default class Gallery extends Component {
 
   renderPage(pageData, pageId, layout) {
     const { onViewTransformed, onTransformGestureReleased, loader, ...other } = this.props;
-    let shouldLoad = pageId == this.currentPage || 
-      pageId == this.currentPage - 1 ||
-      pageId == this.currentPage + 1;
-
-
     const loaded = this.state.imagesLoaded[pageId] && this.state.imagesLoaded[pageId] === true;
     const loadingView = !loaded && loader ? loader : false;
     return (
@@ -292,7 +287,7 @@ export default class Gallery extends Component {
         }).bind(this)}
         key={'innerImage#' + pageId}
         style={{width: layout.width, height: layout.height}}
-        source={shouldLoad ? pageData : null}>
+        source={pageData}>
           { loadingView }
         </Image>
     );
